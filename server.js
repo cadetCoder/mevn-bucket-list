a6df2a1b244e889916b6d5047e109295fb3acf9f
Darrel Toledo <darreltoledo@icloud.com>
2021-11-24 21:37:39 +0000

    declared PORT

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req,res) => res.send(''))
+app.get('/', (req,res) => res.send(''));
+
+app.listen(PORT, () => console.log(`Listening to PORT ${PORT}`));
