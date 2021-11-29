ac421a69f21043314287aadbe837726842e60bdf
Darrel Toledo <darreltoledo@icloud.com>
2021-11-29 21:27:01 +0000

    connected the config file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const mongoose = require('mongoose')
+const { PORT, mongoURI } = require('./config')
 
 app.get('/', (req,res) => res.send(''))
 
