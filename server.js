523937a9e8a6dff5bcec6274e242964b556a6cde
Darrel Toledo <darreltoledo@icloud.com>
2021-11-29 21:29:12 +0000

    mongoose config

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const PORT = 3000
 const mongoose = require('mongoose')
 const { PORT, mongoURI } = require('./config')
 
+mongoose
+    .connect(mongoUri, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log('MongoDB database Connected...'))
+    .catch((err) => console.log(err))
+
 app.get('/', (req,res) => res.send(''))
 
 app.listen(PORT, () => console.log(`Listening to PORT ${PORT}`))
