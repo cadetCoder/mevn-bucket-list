4d08266616272f9902a1569ea070ccdc0f13559e
Darrel Toledo <darreltoledo@icloud.com>
2021-12-01 21:38:50 +0000

    Bucket list item schema in models

---
+++ models/BucketListItem.js
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose';
+
+const BucketListItemSchema = new Schema({
+  description: {
+      type: String,
+      required: true,
+  },
+  date: {
+      type: Date,
+      default: Date.now,
+  },
+})
+
+const BucketListItem = model('bucketListItem', BucketListItemSchema)
+
+module.exports = BucketListItem
