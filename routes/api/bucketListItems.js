a0bd9bed6d501ed921f8141345389f3996014c6e
Darrel Toledo <darreltoledo@icloud.com>
2021-12-03 21:24:22 +0000

    condition if no bucketlist found

--- routes/api/bucketListItems.js
+++ routes/api/bucketListItems.js
@@ -5,4 +5,5 @@ const router = Router()
 
 router.get('/', async (req, res) => {
   const bucketListItems = await BucketListItem.find()
+  if (!bucketListItems) throw new Error('No bucketListItems')
 })
