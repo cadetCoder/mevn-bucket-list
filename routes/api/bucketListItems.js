5c4b629334664c17d5a9ae223cef6c9cb0050327
Darrel Toledo <darreltoledo@icloud.com>
2021-12-04 21:34:33 +0000

    try catch block

--- routes/api/bucketListItems.js
+++ routes/api/bucketListItems.js
@@ -4,10 +4,15 @@ const BucketListItem = require('../../models/BucketListItem')
 const router = Router()
 
 router.get('/', async (req, res) => {
-  const bucketListItems = await BucketListItem.find()
-  if (!bucketListItems) throw new Error('No bucketListItems')
-  const sorted = bucketListItems.sort((a,b) => {
-    return new Date(a.date).getTime() - new Date(b.date).getTime()
-  })
-  res.status(200).json(sorted)
+  try {
+    const bucketListItems = await BucketListItem.find()
+    if (!bucketListItems) throw new Error('No bucketListItems')
+    const sorted = bucketListItems.sort((a,b) => {
+      return new Date(a.date).getTime() - new Date(b.date).getTime()
+    })
+    res.status(200).json(sorted)
+  } catch (error) {
+    
+  }
+ 
 })
