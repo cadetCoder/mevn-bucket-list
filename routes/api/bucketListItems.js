f401557fd7c379df7e3846447d2f1ebd57b7d23d
Darrel Toledo <darreltoledo@icloud.com>
2021-12-06 07:59:11 +0000

    post router debug

--- routes/api/bucketListItems.js
+++ routes/api/bucketListItems.js
@@ -5,24 +5,24 @@ const router = Router()
 
 router.get('/', async (req, res) => {
   try {
-    const bucketListItems = await BucketListItem.find()
-    if (!bucketListItems) throw new Error('No bucketListItems')
-    const sorted = bucketListItems.sort((a,b) => {
-      return new Date(a.date).getTime() - new Date(b.date).getTime()
-    })
-    res.status(200).json(sorted)
+      const bucketListItems = await BucketListItem.find()
+      if (!bucketListItems) throw new Error('No bucketListItems')
+      const sorted = bucketListItems.sort((a, b) => {
+          return new Date(a.date).getTime() - new Date(b.date).getTime()
+      })
+      res.status(200).json(sorted)
   } catch (error) {
-    res.status(500).json({ message: error.message})
+      res.status(500).json({ message: error.message })
   }
 })
 
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
   const newBucketListItem = new BucketListItem(req.body)
   try {
-    const bucketListItem = await newBucketListItem.save()
-    if (!bucketListItem) throw new Error('Something went wrong saving the bucketListItem')
-    res.status(200).son(bucketListItem)
+      const bucketListItem = await newBucketListItem.save()
+      if (!bucketListItem) throw new Error('Something went wrong saving the bucketListItem')
+      res.status(200).json(bucketListItem)
   } catch (error) {
-    
+      res.status(500).json({ message: error.message })
   }
 })
