f54e9769cee2111423791b54b0e4d2951d363a11
Darrel Toledo <darreltoledo@icloud.com>
2021-12-07 22:10:24 +0000

    require bucketlistitem in server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { PORT, mongoUri } = require('./config')
 const cors = require('cors')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const bucketListItemRoutes = require('./routes/api/bucketListItems')
 
 app.use(cors())
 app.use(morgan('tiny'))
