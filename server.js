c818e4be19d18497b8067dd66b72a0c05f763ce5
Darrel Toledo <darreltoledo@icloud.com>
2021-12-22 21:25:54 +0000

    refractore

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const bucketListItemRoutes = require('./routes/api/bucketListItems')
+const path = require('path')
 
 app.use(cors())
 app.use(morgan('tiny'))
@@ -21,7 +22,13 @@ mongoose
     .then(() => console.log('MongoDB database Connected...'))
     .catch((err) => console.log(err))
 
-app.use('./api/bucketListItems', bucketListItemRoutes)
-app.get('/', (req,res) => res.send(''))
+app.use('/api/bucketListItems', bucketListItemRoutes)
 
-app.listen(PORT, () => console.log(`Listening to PORT ${PORT}`))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/dist'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    })
+}
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`))
